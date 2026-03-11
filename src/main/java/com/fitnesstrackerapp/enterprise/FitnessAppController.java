17d260521b3cfecd083af23b77001c529b49898e
dougaler <dougaler@mail.uc.edu>
2026-03-10 22:43:39 -0400

    Added UI functionality

--- src/main/java/com/fitnesstrackerapp/enterprise/FitnessAppController.java
+++ src/main/java/com/fitnesstrackerapp/enterprise/FitnessAppController.java
@@ -1,7 +1,11 @@
 package com.fitnesstrackerapp.enterprise;
 
+import com.fitnesstrackerapp.enterprise.dto.Account;
+import com.fitnesstrackerapp.enterprise.dto.DistanceGoal;
+import com.fitnesstrackerapp.enterprise.dto.RepGoal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class FitnessAppController {
@@ -10,7 +14,29 @@ public class FitnessAppController {
      *
      */
     @RequestMapping("/")
-    public String index(){
-     return ("start");
+    public String index(Model model){
+        //Account Attribute
+        Account account = new Account();
+        account.setAccountId(1);
+        account.setAccountName("Fred");
+        account.setEmail("Fred@email");
+        account.setPassword("password");
+        model.addAttribute("account", account);
+
+        //Distance Goal Attribute
+        DistanceGoal dGoal = new DistanceGoal();
+        dGoal.setGoalType("Miles");
+        dGoal.setDistanceToComplete(5);
+        dGoal.setDistanceCompleted(3);
+        model.addAttribute("dGoal", dGoal);
+
+        //Distance Goal Attribute
+        RepGoal repGoal = new RepGoal();
+        repGoal.setGoalType("Sit Ups");
+        repGoal.setRepsToComplete(50);
+        repGoal.setRepsCompleted(32);
+        model.addAttribute("repGoal", repGoal);
+
+        return ("start");
     }
 }
