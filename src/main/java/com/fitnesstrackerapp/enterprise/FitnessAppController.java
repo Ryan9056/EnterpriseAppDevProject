dbab2a5cbdcdbcfaf145bf98253cea430815adbd
dougaler <113709222+dougaler@users.noreply.github.com>
2026-03-12 15:46:50 -0400

    Added UI functionality (#10)
    * Added UI functionality
    
    * Added new page, and attempted to fix some formatting

--- src/main/java/com/fitnesstrackerapp/enterprise/FitnessAppController.java
+++ src/main/java/com/fitnesstrackerapp/enterprise/FitnessAppController.java
@@ -1,7 +1,12 @@
 package com.fitnesstrackerapp.enterprise;
 
+import com.fitnesstrackerapp.enterprise.dto.Account;
+import com.fitnesstrackerapp.enterprise.dto.DistanceGoal;
+import com.fitnesstrackerapp.enterprise.dto.RepGoal;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class FitnessAppController {
@@ -10,7 +15,39 @@ public class FitnessAppController {
      *
      */
     @RequestMapping("/")
-    public String index(){
-     return ("start");
+    public String index(Model model){
+        //Account Attributes For Ui
+        //Example Data for now
+        Account account = new Account();
+        account.setAccountId(1);
+        account.setAccountName("Fred");
+        account.setEmail("Fred@email");
+        account.setPassword("password");
+        model.addAttribute("account", account);
+
+        //Distance Goal Attributes For Ui
+        //Example Data for now
+        DistanceGoal dGoal = new DistanceGoal();
+        dGoal.setGoalType("Miles");
+        dGoal.setDistanceToComplete(5);
+        dGoal.setDistanceCompleted(3);
+        model.addAttribute("dGoal", dGoal);
+
+        //Distance Goal Attribute For Ui
+        //Example Data for now
+        RepGoal repGoal = new RepGoal();
+        repGoal.setGoalType("Sit Ups");
+        repGoal.setRepsToComplete(50);
+        repGoal.setRepsCompleted(32);
+        model.addAttribute("repGoal", repGoal);
+
+        return ("start");
     }
+
+    //Created Page map for the UI
+    @GetMapping("/createGoal")
+    public String createGoalPage() {
+        return "createGoal";
+    }
+
 }
