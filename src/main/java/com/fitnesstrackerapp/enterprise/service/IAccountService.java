ceab2f4d964a5b3f43f35b575b58a925cfc12f12
Talic72 <EMORRISWORK72@GMAIL.COM>
2026-03-15 17:53:33 -0400

    Added JavaDoc documentation to IAccountService.java interface methods

--- src/main/java/com/fitnesstrackerapp/enterprise/service/IAccountService.java
+++ src/main/java/com/fitnesstrackerapp/enterprise/service/IAccountService.java
@@ -1,10 +1,27 @@
 package com.fitnesstrackerapp.enterprise.service;
 
 import com.fitnesstrackerapp.enterprise.dto.Account;
+/**
+ * Service interface responsible for managing Account objects.
+ * Defines methods for retrieving and saving the accounts data.
+ */
 
 public interface IAccountService {
+        /**
+     * Retrieves an account using its unique identifier.
+     *
+     * @param accountId the id of the account to retrieve
+     * @return the account object associated with the provided id
+     */
 
     Account fetchById(int accountId);
+
+    /**
+     * Saves a new account or updates an existing account.
+     *
+     * @param account the account to save
+     * @return the saved account object
+     */
 
     Account save(Account account);
 }
