65f9011380c542b035d988d2ec9303f387a8a718
dougaler <dougaler@mail.uc.edu>
2026-04-20 20:09:00 -0400

    new pages along with database retrieval

--- src/main/java/com/fitnesstrackerapp/enterprise/FitnessAppController.java
+++ src/main/java/com/fitnesstrackerapp/enterprise/FitnessAppController.java
@@ -2,11 +2,26 @@ package com.fitnesstrackerapp.enterprise;
 
 import com.fitnesstrackerapp.enterprise.dto.Account;
 import com.fitnesstrackerapp.enterprise.dto.DistanceGoal;
+import com.fitnesstrackerapp.enterprise.dto.Goal;
 import com.fitnesstrackerapp.enterprise.dto.RepGoal;
+import com.fitnesstrackerapp.enterprise.service.IGoalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.io.IOException;
 
 @Controller
 public class FitnessAppController {
@@ -17,6 +32,9 @@ public class FitnessAppController {
      * Handle the root (/) endpoint and return a start page @return (currently no start page)
      *
      */
+    @Autowired
+    IGoalService goalService;
+
     @RequestMapping("/")
     public String index(Model model){
         //Account Attributes For Ui
@@ -28,23 +46,45 @@ public class FitnessAppController {
         account.setPassword(DEFAULT_PASSWORD);
         model.addAttribute("account", account);
 
-        //Distance Goal Attributes For Ui
-        //Example Data for now
+        List<Goal> goals = new ArrayList<>();
+
         DistanceGoal dGoal = new DistanceGoal();
         dGoal.setGoalType("Miles");
         dGoal.setDistanceToComplete(5);
         dGoal.setDistanceCompleted(3);
-        model.addAttribute("dGoal", dGoal);
+        goals.add(dGoal);
 
-        //Distance Goal Attribute For Ui
-        //Example Data for now
         RepGoal repGoal = new RepGoal();
         repGoal.setGoalType("Sit Ups");
         repGoal.setRepsToComplete(50);
         repGoal.setRepsCompleted(32);
-        model.addAttribute("repGoal", repGoal);
+        goals.add(repGoal);
 
-        return ("start");
+        //List<Goal> goals = goalService.fetchActiveGoals(account.getAccountId());
+        model.addAttribute("goals", goals);
+
+        return "start";
+    }
+
+    @RequestMapping("/saveGoal")
+    public String saveGoal(Goal goal){
+        try {
+            goalService.save(goal);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return "start";
+        }
+
+        return "start";
+    }
+
+    @GetMapping("/viewGoal")
+    public ResponseEntity viewGoalPage(@RequestParam(value="goal", required=true, defaultValue="None") int goalId) {
+        Goal goal = goalService.fetchById(goalId);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return new ResponseEntity(goal, headers, HttpStatus.OK);
     }
 
     //Created Page map for the UI
@@ -53,4 +93,22 @@ public class FitnessAppController {
         return "createGoal";
     }
 
+    @GetMapping("/account")
+    public String accountPage() {
+        return "account";
+    }
+
+    @GetMapping("/start")
+    public String startPage() {
+        return "start";
+    }
+
+    @GetMapping("/allGoal")
+    public String allGoalPage() {
+        return "allGoal";
+    }
+
+
+
 }
+
